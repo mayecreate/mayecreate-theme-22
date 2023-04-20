6955c2d67cdbd88fdc2a525ad1f700bc1c0e856b
ernstt@umsystem.edu <ernstt@umsystem.edu>
2023-04-20 16:44:43 -0500

    More ADA and life fixes

--- partials/content-social.php
+++ partials/content-social.php
@@ -5,7 +5,8 @@ if( $social_media ) {
     foreach( $social_media as $media ) {
         $social_site = $media['social_site'];
         $social_icon = $media['social_icon'];
-		echo '<li><a target="_blank" href="' . $social_site .'"><i class="fa-brands fab fa-' . $social_icon . '"></i></a></li>';
+        $social_title = $media['social_title'];
+		echo '<li><a target="_blank" href="' . $social_site .'" title="Social Media Link for '.$social_title.'"><i class="fa-brands fab fa-' . $social_icon . '" title="Social Media Icon for '.$social_title.'"></i></a></li>';
     }
     echo '</ul>';
 } ?>
